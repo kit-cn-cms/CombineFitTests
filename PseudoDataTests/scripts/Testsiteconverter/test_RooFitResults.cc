f146f2b0a83b19496c677f844c09abea807f2ed6
David Fischer <david.fischer2@student.kit.edu>
2017-09-08 15:40:13 +0200

    First test, able to create new TFiles, with right values

--- PseudoDataTests/scripts/Testsiteconverter/test_RooFitResults.cc
+++ PseudoDataTests/scripts/Testsiteconverter/test_RooFitResults.cc
@@ -5,10 +5,12 @@
 #include "iostream"
 #include "iomanip"
 #include "RooFitResult.h"
+#include "RooArgSet.h"
 #include "RooRealVar.h"
 #include "TTree.h"
 #include "TFolder.h"
 #include "TString.h"
+
 using namespace std;
 
 void test(TString filename = "mlfit.root")
@@ -16,41 +18,55 @@ void test(TString filename = "mlfit.root")
 	// Reading of data
 	TFile* file = new TFile(filename.Data());
 	std::cout << "opened file\n";
+	// Checking whether file exists and has data saved in it
 	if (file->IsOpen() && !file->IsZombie() && !file->TestBit(TFile::kRecovered) )
 	{
-	// Initalizing fit_b
-	RooFitResult* fit_b = (RooFitResult*) file->Get("fit_b");
-	std::cout << "loaded fit_b\n";
-	// Iterator for fit values
-	TIter Itpostb = fit_b->floatParsInit().createIterator();
-	std::cout << "created iterator\n";
+		// Initalizing fitb
+		RooFitResult* fitb;
+		file->GetObject("fit_s",fitb);
+		std::cout << "loaded fit_b\n";
 
-	// Variables for nuisances
-	RooRealVar* Varpostb;
-	TFile* output = new TFile("test.root", "RECREATE");
-	// Tree to save values from Fit
-	TFolder* Fpostb = new TFolder("Tpostb","Nuisances postfit,only background");
-	std::cout << "created folder " << Fpostb->GetName() << std::endl;
-	// Test for Iterator
+		// Iterator for fit values
+		std::cout << "created iterator\n";
+		TIter Ipostb = fitb->floatParsFinal().createIterator();
 
-	while((Varpostb = (RooRealVar*) Itpostb.Next()))
-	{
-                double_t val, err, hie, loe;
-		std::cout << "loading variable " << Varpostb->GetName() << std::endl;
-		TTree* Ttemp = new TTree(Varpostb->GetName(),Varpostb->GetName());
-		Ttemp->Branch("Value",&val,"value/D");
-		Ttemp->Branch("Error",&err,"error/D");
-		Ttemp->Branch("HighError",&hie,"higherror/D");
-		Ttemp->Branch("LowError",&loe,"lowerrorii/D");
-		val = Varpostb->getVal();
-                err = Varpostb->getError();
-                hie = Varpostb->getErrorHi();
-                loe = Varpostb->getErrorLo();
-		Ttemp->Fill();
-		Fpostb->Add(Ttemp);
-	}
+		// Creating Folder for Trees
+		TFolder* Fpostb = new TFolder("test","Nuisances postfit,only background");
 
-	Fpostb->Write();
-	output->Close();
+		// Creating Varpostb to temporarily save data
+
+		RooRealVar* Varpostb;
+
+		// Opening new TFile to save data
+
+		TFile * output= new TFile("test.root","RECREATE");
+
+		while((Varpostb = (RooRealVar*) Ipostb.Next()))
+		{
+			// Creating variables for Branches
+			double val = -999;
+			double err = -999;
+			double hie = -999;
+			double loe = -999;
+			std::cout << "loading variable " << Varpostb->GetName() << std::endl;
+			// Creating Tree for prozess to save values in Branches
+			TTree* Ttemp = new TTree(Varpostb->GetName(),Varpostb->GetName());
+			// Creating Branches for Tree with specific data
+			Ttemp->Branch("Value",&val,"value/D");
+			Ttemp->Branch("Error",&err,"error/D");
+			Ttemp->Branch("HighError",&hie,"higherror/D");
+			Ttemp->Branch("LowError",&loe,"lowerrorii/D");
+			val = Varpostb->getVal();
+                	err = Varpostb->getError();
+                	hie = Varpostb->getErrorHi();
+                	loe = Varpostb->getErrorLo();
+                
+			Ttemp->Fill();
+			// Adding Trees to folder
+			Fpostb->Add(Ttemp);
+		}
+		// Writing data in new File
+		Fpostb->Write();
+		output->Close();
 	}
 }
