a6ca418ae14448850f2f15c8ab3e5dd1aca3c5e6
David Fischer <david.fischer2@student.kit.edu>
2017-09-08 13:59:42 +0200

    First test, able to create TFolder, but wrong values

--- PseudoDataTests/scripts/Testsiteconverter/test_RooFitResults.cc
+++ PseudoDataTests/scripts/Testsiteconverter/test_RooFitResults.cc
@@ -1,6 +1,5 @@
 #include "TROOT.h"
-#include "TChain.h"
-#include "TH1F.h"
+#include "TH1D.h"
 #include "TF1.h"
 #include "TFile.h"
 #include "iostream"
@@ -8,39 +7,50 @@
 #include "RooFitResult.h"
 #include "RooRealVar.h"
 #include "TTree.h"
-
+#include "TFolder.h"
+#include "TString.h"
 using namespace std;
 
-int main()
+void test(TString filename = "mlfit.root")
 {
 	// Reading of data
-	TFile* file = new TFile("mlfit.root");
-
+	TFile* file = new TFile(filename.Data());
+	std::cout << "opened file\n";
+	if (file->IsOpen() && !file->IsZombie() && !file->TestBit(TFile::kRecovered) )
+	{
 	// Initalizing fit_b
 	RooFitResult* fit_b = (RooFitResult*) file->Get("fit_b");
-
+	std::cout << "loaded fit_b\n";
 	// Iterator for fit values
-	TIter* Itpostb = (TIter*) fit_b->floatParsInit().createIterator();
-
+	TIter Itpostb = fit_b->floatParsInit().createIterator();
+	std::cout << "created iterator\n";
 
 	// Variables for nuisances
 	RooRealVar* Varpostb;
-
+	TFile* output = new TFile("test.root", "RECREATE");
 	// Tree to save values from Fit
-	TTree* Tpostb = new TTree("Tpostb","Nuisances postfit,only background");
-
+	TFolder* Fpostb = new TFolder("Tpostb","Nuisances postfit,only background");
+	std::cout << "created folder " << Fpostb->GetName() << std::endl;
 	// Test for Iterator
-	while((Varpostb = (RooRealVar*) Itpostb->Next()))
+
+	while((Varpostb = (RooRealVar*) Itpostb.Next()))
 	{
-		double_t cat[4];
-		Tpostb->Branch(Varpostb->GetName(),cat,"cat[3]/D");
-		cat[0] = Varpostb->getVal();
-		cat[1] = Varpostb->getError();
-		cat[2] = Varpostb->getErrorHi();
-		cat[3] = Varpostb->getErrorLo();
-		Tpostb->Fill();
+                double_t val, err, hie, loe;
+		std::cout << "loading variable " << Varpostb->GetName() << std::endl;
+		TTree* Ttemp = new TTree(Varpostb->GetName(),Varpostb->GetName());
+		Ttemp->Branch("Value",&val,"value/D");
+		Ttemp->Branch("Error",&err,"error/D");
+		Ttemp->Branch("HighError",&hie,"higherror/D");
+		Ttemp->Branch("LowError",&loe,"lowerrorii/D");
+		val = Varpostb->getVal();
+                err = Varpostb->getError();
+                hie = Varpostb->getErrorHi();
+                loe = Varpostb->getErrorLo();
+		Ttemp->Fill();
+		Fpostb->Add(Ttemp);
 	}
 
-	Tpostb->Scan();
-	return 0;
+	Fpostb->Write();
+	output->Close();
+	}
 }
