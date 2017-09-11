dab3cd4a48cce1b195c9d90b34529ff9fc83b116
David Fischer <david.fischer2@student.kit.edu>
2017-09-11 11:46:43 +0200

    Second testprogramm, right values, though worth improving slightly

--- PseudoDataTests/scripts/Testsiteconverter/test_correlation.cc
+++ PseudoDataTests/scripts/Testsiteconverter/test_correlation.cc
@@ -25,32 +25,40 @@ void test1(TString filename = "mlfit.root", bool mod =0)
 		else file->GetObject("fit_s",fitb);
 		
 		std::cout << "loaded data\n";
+	        TFile* output = new TFile("testcor.root","RECREATE");
 
 		// Initilaising Folder to save values
 		TFolder * Fpostb = new TFolder("Correlation", "Correlation");
 
 		// Creating iterators
 		TIter Ipostb_one = fitb->floatParsFinal().createIterator();
-		TIter Ipostb_two = fitb->floatParsFinal().createIterator();
 		std::cout << "created iterators\n";
 
 		// Initialising Vars to temporarily save data
 		RooRealVar* Varpostb_one;
 		RooRealVar* Varpostb_two;
-
+		TTree* Tpostb = NULL;
+		int count;
 		while((Varpostb_one = (RooRealVar*) Ipostb_one.Next()))
 		{
-			TTree* Tpostb = new TTree(Varpostb_one->GetName(),Varpostb_one->GetName());
+			count = 0;
+			Double_t* values = new Double_t[fitb->floatParsFinal().getSize()];
+			std::cout << "before creating new TTree: Tpostb = " << Tpostb <<std::endl;
+			Tpostb = new TTree(Varpostb_one->GetName(),Varpostb_one->GetName());
+			std::cout << "after creating new TTree: Tpostb = " << Tpostb <<std::endl;
+			TIter Ipostb_two = fitb->floatParsFinal().createIterator();	
 			while((Varpostb_two=(RooRealVar*) Ipostb_two.Next()))
 			{
-				Double_t value = -999;
-				Tpostb->Branch(Varpostb_two->GetName(),&value, "correlation/D");
-				value = fitb->correlation(Varpostb_one->GetName(),Varpostb_two->GetName());
-				Tpostb->Fill();
+				values[count] = fitb->correlation(Varpostb_one->GetName(), Varpostb_two->GetName());
+				Tpostb->Branch(Varpostb_two->GetName(), &values[count], "correlation/D");
+				std::cout << "filling for parameter " << Varpostb_two->GetName()  << " value " << values[count] << " into tree " << Tpostb->GetName() << std::endl;
+				count++;
 			}
-		Fpostb->Add(Tpostb);
+			for(int i=0; i<count; i++) std::cout << "\t" << values[i] << std::endl;
+			Tpostb->Fill();
+			Fpostb->Add(Tpostb);
+			delete[] values;
 		}
-	TFile* output = new TFile("testcor.root","RECREATE");
 	Fpostb->Write();
 	output->Close();
 	}
