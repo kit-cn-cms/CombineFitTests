ab1b5b9638affae53356301800dbcd99c93278a9
Philip Daniel Keicher <philip.keicher@student.kit.edu>
2017-10-24 15:25:09 +0200

    generate nll scan for parameter list in txt file

---
+++ PseudoDataTests/scripts/plotting/doNllscanFromTxt.py
@@ -0,0 +1,32 @@
+import os
+import sys
+import glob
+import subprocess
+
+scriptDir = os.path.dirname(sys.argv[0])
+wildcard = sys.argv[1]
+pathToDatacards = sys.argv[2]
+cmdList = None
+if len(sys.argv) > 3:
+    cmdList = sys.argv[3:]
+    
+basepath = os.getcwd()
+if os.path.exists(pathToDatacards):
+    pathToDatacards = os.path.abspath(pathToDatacards)
+    for txtfilepath in glob.glob(wildcard):
+	txtfilepath = os.path.abspath(txtfilepath)
+	parentDir = os.path.dirname(txtfilepath)
+	datacardname = os.path.basename(parentDir)
+	os.chdir(parentDir)
+	with open(txtfilepath) as f:
+	    lines = f.readlines()
+	    for param in lines:
+		cmd = "python {0}/nllscan.py".format(scriptDir)
+		cmd += " -d {0}/{1}.root".format(pathToDatacards, datacardname)
+		cmd += ' -x {0} --addFitCommand "--redefineSignalPOIs {0}"'.format(param)
+		cmd += " -n _" + datacardname
+		if cmdList:
+		    cmd += " " + " ".join(cmdList)
+		print cmd
+		subprocess.call([cmd], shell=True)
+	os.chdir(basepath)
