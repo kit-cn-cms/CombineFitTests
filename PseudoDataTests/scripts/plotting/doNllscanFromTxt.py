f8cf96e34f23c7eec28bb4971d20526839927c80
Philip Daniel Keicher <philip.keicher@student.kit.edu>
2017-10-26 09:52:18 +0200

    implemented more output

--- PseudoDataTests/scripts/plotting/doNllscanFromTxt.py
+++ PseudoDataTests/scripts/plotting/doNllscanFromTxt.py
@@ -9,7 +9,12 @@ pathToDatacards = sys.argv[2]
 cmdList = None
 if len(sys.argv) > 3:
     cmdList = sys.argv[3:]
-    
+
+print "input:"
+print "\twildcard:", wildcard
+print "\tpathToDatacards:", pathToDatacards
+print "\tcmdList:\n", cmdList
+
 basepath = os.getcwd()
 if os.path.exists(pathToDatacards):
     pathToDatacards = os.path.abspath(pathToDatacards)
@@ -19,8 +24,9 @@ if os.path.exists(pathToDatacards):
 	datacardname = os.path.basename(parentDir)
 	os.chdir(parentDir)
 	with open(txtfilepath) as f:
-	    lines = f.readlines()
+	    lines = f.read().splitlines()
 	    for param in lines:
+		print param
 		cmd = "python {0}/nllscan.py".format(scriptDir)
 		cmd += " -d {0}/{1}.root".format(pathToDatacards, datacardname)
 		cmd += ' -x {0} --addFitCommand "--redefineSignalPOIs {0}"'.format(param)
