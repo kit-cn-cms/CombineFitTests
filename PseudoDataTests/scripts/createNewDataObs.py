0217b8bd7dbab78d9c77dc0cde77ea2fb6c16209
Philip Daniel Keicher <philip.keicher@student.kit.edu>
2017-11-10 11:31:25 +0100

    implemented complete list of categories

--- PseudoDataTests/scripts/createNewDataObs.py
+++ PseudoDataTests/scripts/createNewDataObs.py
@@ -7,7 +7,84 @@ ROOT.gROOT.SetBatch(True)
 inpath = os.path.abspath(sys.argv[1])
 
 listOfProcesses = ["ttbarOther", "ttbarPlusB", "ttbarPlus2B", "ttbarPlusBBbar", "ttbarPlusCCbar"]
-categories = ["ljets_j4_t2", "ljets_j4_t3", "ljets_j4_t4", "ljets_j5_t2", "ljets_j5_t3", "ljets_j5_tge4", "ljets_jge6_t2", "ljets_jge6_t3", "ljets_jge6_tge4"]
+# listOfProcesses = "ttbarOther ttbarPlusB ttbarPlus2B ttbarPlusBBbar ttbarPlusCCbar singlet wjets zjets ttbarW ttbarZ diboson".split()
+# categories = ["ljets_j4_t2", "ljets_j4_t3", "ljets_j4_t4", "ljets_j5_t2", "ljets_j5_t3", "ljets_j5_tge4", "ljets_jge6_t2", "ljets_jge6_t3", "ljets_jge6_tge4"]
+
+categories = [  "ljets_j4_t4", 
+                "ljets_j5_tge4", 
+                "ljets_jge6_t3", 
+                "ljets_jge6_tge4",
+                "ljets_j5_t3",
+                "ljets_j4_t3",
+                "ljets_jge6_t2",
+                
+                "ljets_j4_t4_high", 
+                "ljets_j5_tge4_high", 
+                "ljets_jge6_t3_high", 
+                "ljets_jge6_tge4_high",
+                # "ljets_j5_t3_high",
+                # "ljets_j4_t3_high",
+                
+                "ljets_j4_t4_low", 
+                "ljets_j5_tge4_low", 
+                "ljets_jge6_t3_low", 
+                "ljets_jge6_tge4_low",
+                # "ljets_j5_t3_low",
+                # "ljets_j4_t3_low",
+                
+                "ljets_j4_t4_MEMONLY", 
+                "ljets_j5_tge4_MEMONLY", 
+                "ljets_jge6_t3_MEMONLY", 
+                "ljets_jge6_tge4_MEMONLY",
+                # "ljets_j5_t3_MEMONLY",
+                # "ljets_j4_t3_MEMONLY"
+                
+                "ljets_j4_t4_ttbbOpt",
+                "ljets_j5_tge4_ttbbOpt",
+                "ljets_jge6_t3_ttbbOpt",
+                "ljets_jge6_tge4_ttbbOpt",
+                
+                "ljets_j4_t4_ttbbOpt_high",
+                "ljets_j5_tge4_ttbbOpt_high", 
+                "ljets_jge6_t3_ttbbOpt_high",
+                "ljets_jge6_tge4_ttbbOpt_high",
+                
+                "ljets_j4_t4_ttbbOpt_low",
+                "ljets_j5_tge4_ttbbOpt_low", 
+                "ljets_jge6_t3_ttbbOpt_low",
+                "ljets_jge6_tge4_ttbbOpt_low",
+                
+                "ljets_j4_t3_BLR",
+                "ljets_j5_t3_BLR",
+                "ljets_jge6_t3_BLR",
+                "ljets_jge6_t2_BLR",
+                
+                "ljets_j4_tge3_ttHnode",
+                "ljets_j5_tge3_ttHnode",
+                "ljets_jge6_tge3_ttHnode",
+                
+                "ljets_j4_tge3_ttbbnode",
+                "ljets_j5_tge3_ttbbnode",
+                "ljets_jge6_tge3_ttbbnode",
+                
+                "ljets_j4_tge3_ttbnode",
+                "ljets_j5_tge3_ttbnode",
+                "ljets_jge6_tge3_ttbnode",
+                
+                "ljets_j4_tge3_tt2bnode",
+                "ljets_j5_tge3_tt2bnode",
+                "ljets_jge6_tge3_tt2bnode",
+                
+                "ljets_j4_tge3_ttccnode",
+                "ljets_j5_tge3_ttccnode",
+                "ljets_jge6_tge3_ttccnode",
+                
+                "ljets_j4_tge3_ttlfnode",
+                "ljets_j5_tge3_ttlfnode",
+                "ljets_jge6_tge3_ttlfnode"
+                
+                ] #list of categories used for the fit
+
 suffix = "all_Bkg_"
 key = "$PROCESS_finaldiscr_$CHANNEL"
 
