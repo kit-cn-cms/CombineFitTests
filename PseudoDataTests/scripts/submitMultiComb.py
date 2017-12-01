c8b0bfca865d54b1ea44b99893ef08d92ab4d3a9
Philip Daniel Keicher <philip.keicher@student.kit.edu>
2017-12-01 08:49:05 +0100

    script is now able to handle system wildcards

--- PseudoDataTests/scripts/submitMultiComb.py
+++ PseudoDataTests/scripts/submitMultiComb.py
@@ -8,10 +8,11 @@ ROOT.gROOT.SetBatch(True)
 
 workdir = "/nfs/dust/cms/user/pkeicher/tth_analysis_study/CombineFitTests/PseudoDataTests/scripts"
 
-wildcard = sys.argv[1]
-for inputFile in glob.glob(wildcard):
-    inputFile = os.path.abspath(inputFile)
-    print "submitting data in path", inputFile
-    command=[workdir+"/submitScript.sh", inputFile, inputFile+"/temp/temp_shape_expectation.root"]
-    subprocess.check_call(command)
+wildcards = sys.argv[1:]
+for wildcard in wildcards:
+	for inputFile in glob.glob(wildcard):
+    		inputFile = os.path.abspath(inputFile)
+    		print "submitting data in path", inputFile
+    		command=[workdir+"/submitScript.sh", inputFile, inputFile+"/temp/temp_shape_expectation.root"]
+    		subprocess.check_call(command)
 print "done"
