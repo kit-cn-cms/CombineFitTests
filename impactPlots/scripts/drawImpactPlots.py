dd1e41489a2933175c87083b9a29ec1912c20434
Philip Daniel Keicher <philip.keicher@student.kit.edu>
2017-12-21 11:13:47 +0100

    stuff

--- impactPlots/scripts/drawImpactPlots.py
+++ impactPlots/scripts/drawImpactPlots.py
@@ -10,33 +10,33 @@ datacardDir = os.path.abspath(datacardDir)
 if os.path.exists(datacardDir):
     basepath = os.getcwd()
     for wildcard in wildcards:
-	for directory in glob.glob(wildcard):
-	    directory = os.path.abspath(directory)
-	    os.chdir(directory)
-	    print "changing into", directory
-	
-	    workspace = datacardDir + "/" + os.path.basename(directory) + ".root"
-	    print "checking for file", workspace
-	    if os.path.exists(workspace):
-		print "creating impact plots for directory", directory
-		taskname = os.path.basename(workspace).replace(".root", "")
-		impactName = os.path.basename(directory) + "_impacts"
-		cmd = "combineTool.py -M Impacts -m 125"
-		cmd += " -o " + impactName +".json"
-		cmd += " -d " + workspace
-		cmd += " -n " + taskname
-		print cmd
-		subprocess.call([cmd], shell=True)
-		
-		if os.path.exists(impactName +".json"):
-		    cmd = "plotImpacts.py -i " + impactName +".json"
-		    cmd += " -o " + impactName + " --transparent"
-		    print cmd
-		    subprocess.call([cmd], shell=True)
-		else:
-		    print "could not find .json file"
-	    else:
-		print "could not find workspace in", workspace
-	    os.chdir(basepath)
+        for directory in glob.glob(wildcard):
+            directory = os.path.abspath(directory)
+            os.chdir(directory)
+            print "changing into", directory
+        
+            workspace = datacardDir + "/" + os.path.basename(directory) + ".root"
+            print "checking for file", workspace
+            if os.path.exists(workspace):
+                print "creating impact plots for directory", directory
+                taskname = os.path.basename(workspace).replace(".root", "")
+                impactName = os.path.basename(directory) + "_impacts"
+                cmd = "combineTool.py -M Impacts -m 125"
+                cmd += " -o " + impactName +".json"
+                cmd += " -d " + workspace
+                cmd += " -n " + taskname
+                print cmd
+                subprocess.call([cmd], shell=True)
+                
+                if os.path.exists(impactName +".json"):
+                    cmd = "plotImpacts.py -i " + impactName +".json"
+                    cmd += " -o " + impactName + " --transparent"
+                    print cmd
+                    subprocess.call([cmd], shell=True)
+                else:
+                    print "could not find .json file"
+            else:
+                print "could not find workspace in", workspace
+                os.chdir(basepath)
 else:
     sys.exit("path to datacards does not exist!")
