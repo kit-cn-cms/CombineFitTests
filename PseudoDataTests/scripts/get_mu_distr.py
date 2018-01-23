ed10a0c00608572eda960d879e878f13826236eb
Philip Daniel Keicher <philip.keicher@student.kit.edu>
2018-01-23 08:46:00 +0100

    script to read tree_fit_sb TTree in fitDiagnostics.root files to quickly create postfit signal strength distributions

---
+++ PseudoDataTests/scripts/get_mu_distr.py
@@ -0,0 +1,30 @@
+from ROOT import TFile, TCanvas, TH1, gStyle, TTree, gROOT
+from os import path
+from sys import argv
+from glob import glob
+
+gROOT.SetBatch(True)
+
+files = argv[1:]
+
+for word in files:
+    for f in glob(word):
+        if not path.exists(f):
+            print "could not file", f
+            continue
+        if not f.endswith(".root"):
+            print "input must be .root file!"
+            continue
+        dirname = path.dirname(path.abspath(f))
+        infile = TFile(f)
+        if infile.IsOpen() and not infile.IsZombie() and not infile.TestBit(TFile.kRecovered):
+            tree_fit_sb = infile.Get("tree_fit_sb")
+            gStyle.SetOptStat(221112211)
+            c = TCanvas()
+            tree_fit_sb.Draw("r")
+            outname = dirname +"/r_" + f
+            outname = outname.replace(".root", ".pdf")
+            c.SaveAs(outname)
+            infile.Close()
+        else:
+            print "file is broken: ", f
