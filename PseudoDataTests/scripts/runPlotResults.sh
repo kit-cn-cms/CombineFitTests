16ded70d4fc5492b468e7a73e07b29caa12a137d
Philip Daniel Keicher <philip.keicher@student.kit.edu>
2018-02-14 18:53:46 +0100

    added input for new option of plotResults.C

--- PseudoDataTests/scripts/runPlotResults.sh
+++ PseudoDataTests/scripts/runPlotResults.sh
@@ -7,6 +7,10 @@ cd $CMSSW_BASE
 eval `scram runtime -sh`
 cd -
 
+if test "$#" -eq 4; then
+  echo "plotResults.C(\""$1"\",\""$2"\", \""$3"\", \""$4"\")"
+  root -l -b -q "plotResults.C(\""$1"\",\""$2"\", \""$3"\", \""$4"\")"
+else
 if test "$#" -eq 3; then
   echo "plotResults.C(\""$1"\",\""$2"\", "$3")"
 
@@ -16,4 +20,5 @@ else
   echo "plotResults.C(\""$1"\",\""$2"\")"
 
   root -l -b -q "plotResults.C+(\""$1"\",\""$2"\")"
+fi
 fi
