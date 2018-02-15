d34d60805a0f1c443e75e64a62b0693325852fe0
David Fischer <david.fischer2@student.kit.edu>
2018-02-15 12:42:57 +0100

    TChaining improved

--- PseudoDataTests/scripts/Testsiteconverter/TChaining.h
+++ PseudoDataTests/scripts/Testsiteconverter/TChaining.h
@@ -4,51 +4,46 @@
 #include "TROOT.h"
 #include "TTree.h"
 #include "TFile.h"
+#include "TDirectory.h"
 
 int chaining(TString Filename = "fitDiagnosics1.root", TString Output = "combined_fits.root")
 {
 	TString firstfile = Filename;
 	while(firstfile.MaybeWildcard())
-	{
-		int aces = firstfile.Index("*",1);
-		firstfile.Replace(aces,1,"1");
-	}
+	{	int aces = firstfile.Index("*",1);
+		firstfile.Replace(aces,1,"1");	}
+
 	// Loading first file as a sample, to dynamically create the right amount of TChains
 	TFile* Samplefile = new TFile(firstfile.Data());
 
 	// Creating File, to store all the collected Trees
-	TFile* output = new TFile(Output.Data(),"RECREATE");
+	TFile* output = TFile::Open(Output.Data(),"RECREATE");
 
 	// Creating sample list
 	TList* Lsamples = (TList*) Samplefile->GetListOfKeys();
 
-	TDirectoryFile* Dlayer1;
-
 	while(Lsamples->GetSize()!=0)
 	{
 		// Temporalily save subdirectory
-		Dlayer1= (TDirectoryFile*) Samplefile->Get(Lsamples->Last()->GetName());
-		TDirectoryFile* Ftemp = new TDirectoryFile(Dlayer1->GetName(),Dlayer1->GetTitle());
-		TList* Lsub = Dlayer1->GetListOfKeys();
-		TTree* Ttemp;
+		TDirectoryFile* Dir = (TDirectoryFile*) Samplefile->Get(Lsamples->Last()->GetName());
+		// Creating subdirectories to save the TTrees from TChains
+		output->mkdir(Lsamples->Last()->GetName(),Lsamples->Last()->GetTitle())->cd();
+		// Finding all the TTrees within the current Directory
+		TList* Lsub = (TList*) Dir->GetListOfKeys();
 
-		while((Lsub->GetSize()!=0)&&(!TString(Dlayer1->Get(Lsub->Last()->GetName())->ClassName()).Contains("TDirectoryFile")))
+		while(	(Lsub->GetSize()!=0)&&(!TString(Lsub->Last()->GetTitle()).Contains("TDirectory")))
 		{
 			// Generating location name for chain
 			TString Spath = TString(Lsamples->Last()->GetName());
-			Spath.Append("/");
-			Spath.Append(Lsub->Last()->GetName());
+			Spath += TString("/") + TString(Lsub->Last()->GetName());
 			// Calling Chain
-			TChain* temp = new TChain(Spath.Data());
-			temp->Add(Filename.Data());
-			// retrieving TTree
-			Ttemp =(TTree*) temp->CloneTree(-1);
-			Ftemp->Add(Ttemp);
+			TChain temp(Spath.Data());
+			temp.Add(Filename.Data());
+			// saving TTree
+			temp.Merge(output,1000,"keep");
 			Lsub->RemoveLast();
 		}
-		Ftemp->Write();
 		Lsamples->RemoveLast();
-		delete Ftemp;
 	}
 	output->Close();
 	return 0;
