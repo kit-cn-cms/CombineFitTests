126909eff43c87951cd6edbe6b1ce8d51cc7977a
Jan van der Linden <jan.linden@student.kit.edu>
2018-05-08 17:59:02 +0200

    lazy update: adjusted scripts to condor_qsub, untested

--- PseudoDataTests/scripts/resubArrayjob.sh
+++ PseudoDataTests/scripts/resubArrayjob.sh
@@ -6,7 +6,7 @@ paths=${@:3}
 
 for path in $paths
 do
-	cmd="qsub -q $queue.q -cwd -terse -t 1-$njobs -S /bin/bash -l h=bird* -hard -l os=sld6 -l h_vmem=2000M -l s_vmem=2000M -o /dev/null -e /dev/null $path"
+	cmd="condor_qsub -cwd -terse -t 1-$njobs -S /bin/bash -o /dev/null -e /dev/null $path"
 	echo $cmd
 	eval $cmd
 done
